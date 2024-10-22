8450b8faab632533d5c86c4cc2b8da32d2af1ba9
Thomas Weaverson <thomasweaverson@gmail.com>
2024-10-22 19:40:30 +0300

    3.6 Переименует страницу offer в room

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import Main from '../../pages/main/main';
-import Offer from '../../pages/offer/offer';
+import Room from '../../pages/room/room';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import Favorites from '../../pages/favorites/favorites';
 import Login from '../../pages/login/login';
@@ -22,14 +22,14 @@ function App( {offersCount}: AppProps): JSX.Element {
 
         <Route
           path={`${AppRoute.Offer}/:id`}
-          element={<Offer />}
+          element={<Room />}
         />
 
         <Route
           path={AppRoute.Favorites}
           element={
             <PrivateRoute
-              authorizationStatus={AuthorizationStatus.Auth}
+              authorizationStatus={AuthorizationStatus.NoAuth}
             >
               <Favorites />
             </PrivateRoute>
